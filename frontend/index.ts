f41f23dbef6db55534c6fc4b405751ffe489bd8e
蔡沛豪 <cph23@mails.tsinghua.edu.cn>
2023-08-03 23:52:05 +0800

    更改Object.keys() + map为Object.values()

--- frontend/index.ts
+++ frontend/index.ts
@@ -88,7 +88,7 @@ async function part2(ups: Up[]) {
 async function main() {
 
 	const bili = await fetch('bili.json').then(res => res.json()) as { [group: string]: Up[] };
-	const ups = Object.keys(bili).map(group => bili[group]).flat()
+	const ups = Object.values(bili).flat();
 	ups.forEach(up => mid2name[up['mid']] = up['name']);
 
 	const [{ infos, alert_list }, videos] = await Promise.all([
