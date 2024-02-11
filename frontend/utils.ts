2595d69acc8e874564c10bdb8407867a6df05f55
蔡沛豪 <cph23@mails.tsinghua.edu.cn>
2024-02-11 11:47:24 +0800

    添加了objectMap

--- frontend/utils.ts
+++ frontend/utils.ts
@@ -9,3 +9,13 @@ function createElement<K extends keyof HTMLElementTagNameMap>(innerHTML: string)
 	if (elems.length !== 1) throw 'CPH: 调用 createElement 时需保证 innerHTML 仅有一个元素';
 	return elems[0];
 }
+
+function objectMap<
+	O extends object,
+	F extends <K extends keyof O>(key: K, value: O[K], index: number) => unknown,
+>(obj: O, func: F): {
+	[K in keyof O]: ReturnType<F>
+} {
+	// @ts-ignore 不必在意，只能假设 F 返回的类型相同了……
+	return Object.fromEntries(Object.entries(obj).map(([key, value], index) => [key, func(key, value, index)]))
+}
