477a372c4fbf5e80d3caae731fa40ba0c4d983e0
蔡沛豪 <cph23@mails.tsinghua.edu.cn>
2024-09-22 22:14:17 +0800

    优化了计数更新 buvid3 的逻辑

--- backend/.utils/genHeader.ts
+++ backend/.utils/genHeader.ts
@@ -20,20 +20,20 @@ type Finger = {
     message: string // 'ok'
 }
 
-var buvid3: string | undefined;
-var count = 0;
 const MAX_COUNT = 100;
+var buvid3: string; // 暂未初始化
+var count = MAX_COUNT; // 保证现在处于失效状态，从而初始化 buvid3
 
 export async function genHeader() {
-	if (typeof buvid3 === 'undefined' || count >= MAX_COUNT) {
+	// 若 count = 0 表示已有协程在初始化了…… 等待即可
+	while (count === 0) await new Promise(resolve => setTimeout(resolve));
+	// 若超过 MAX_COUNT 初始化
+	if (count >= MAX_COUNT) {
+		if (count > MAX_COUNT) throw '断言失败，代码写的不好啊/doge'
 		count = 0; // 提示已有协程在更新 buvid3
 		const res = await myFetch('https://api.bilibili.com/x/frontend/finger/spi');
-		const finger = JSON.parse(res) as Promise<Finger>;
+		const finger = JSON.parse(res) as Finger;
 		buvid3 = finger['data']['b_3'];
-	} else if (count === 0) { // 若 count = 0 表示已有协程在初始化了…… 等待即可
-		do {
-			await new Promise(resolve => setTimeout(resolve));
-		} while (count === 0);
 	}
 	++count; // 也会提示完成更新 buvid3
 	return ({
